20bd54213dfd67d325292dc408b8620dfe520c43
Peter <p.dyumin@dunice.net>
2023-01-20 22:27:13 +0300

    Initial

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import {
+  onMounted,
+  reactive,
+  ref,
+} from 'vue';
+
+interface ComponentState {
+  ctx: CanvasRenderingContext2D | null;
+}
+
+const state = reactive<ComponentState>({
+  ctx: null,
+})
+
+const canvasRef = ref<HTMLCanvasElement>();
+
+const draw = (video: HTMLVideoElement): null | void => {
+  const { ctx } = state;
+  if (!ctx) {
+    return null;
+  }
+
+  ctx.drawImage(video, 0, 0);
+  setTimeout(draw, 10, video);
+}
+
+const handleError = (error: MediaStreamError): void => {
+  console.log(error);
+}
+
+const handleSuccess = (stream: MediaStream): null | void => {
+  const video = document.createElement('video');
+  video.onplay = (): null | void => draw(video)
+  video.srcObject = stream;
+  video.play();
+}
+
+onMounted((): void => {
+  if (canvasRef.value) {
+    canvasRef.value.height = window.innerHeight;
+    canvasRef.value.width = window.innerWidth;
+    state.ctx = canvasRef.value.getContext('2d');
+  }
+
+  navigator.getUserMedia(
+    {
+      audio: false,
+      video: true,
+    },
+    handleSuccess,
+    handleError,
+  );
+});
+</script>
+
+<template>
+  <div class="f j-center ai-center wrap">
+    <canvas ref="canvasRef" />
+  </div>
+</template>
+
+<style scoped>
+canvas {
+  max-height: 100vh;
+  max-width: 100vw;
+}
+.wrap {
+  height: 100vh;
+  width: 100%;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+// eslint-disable-next-line
+import 'common-styles/styles.css';
+
+import App from './App.vue';
+import './style.css';
+
+createApp(App).mount('#app');
