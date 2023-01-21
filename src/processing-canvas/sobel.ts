b31cb3a67c872fc1054ce825678a8679d86ec76d
Peter <p.dyumin@dunice.net>
2023-01-21 23:58:28 +0300

    Work on Sobel filter

--- src/processing-canvas/sobel.ts
+++ src/processing-canvas/sobel.ts
@@ -1,4 +1,9 @@
-// reference: https://github.com/julyskies/brille/blob/release/processing/solarize.go
+// reference: https://github.com/julyskies/brille/blob/release/processing/sobel-filter.go
+
+interface Point {
+  x: number;
+  y: number;
+}
 
 const HORIZONTAL: number[][] = [
   [-1, 0, 1],
@@ -12,20 +17,50 @@ const VERTICAL: number[][] = [
   [-1, -2, -1],
 ];
 
+const getCoordinates = (
+  position: number,
+  width: number,
+  height: number,
+): Point => ({
+  x: position % width,
+  y: position % height,
+});
+
+const getGradientPoint = (
+  axisValue: number,
+  shift: number,
+  axisLength: number,
+): number => {
+	if (axisValue + shift >= axisLength) {
+		return axisLength - axisValue - 1;
+	}
+	return shift
+}
+
 export default function sobel(imageData: ImageData): ImageData {
   const { data, height, width } = imageData;
   const newImageData = new ImageData(imageData.width, imageData.height);
   for (let i = 0; i < data.length; i += 4) {
-    // newImageData.data[i] = data[i] <= adjustedThreshold
-    //   ? 255 - data[i]
-    //   : data[i];
-    // newImageData.data[i + 1] = data[i + 1] <= adjustedThreshold
-    //   ? 255 - data[i + 1]
-    //   : data[i + 1];
-    // newImageData.data[i + 2] = data[i + 2] <= adjustedThreshold
-    //   ? 255 - data[i + 2]
-    //   : data[i + 2];
-    // newImageData.data[i + 3] = data[i + 3];
+    const { x, y } = getCoordinates(i, width, height);
+    let gradientX = 0;
+    let gradientY = 0;
+    for (let m = 0; m < 3; m += 1) {
+      for (let n = 0; n < 3; n += 1) {
+        const k = getGradientPoint(x, m, width)
+        const l = getGradientPoint(y, n, height)
+        grayColor, _ := utilities.Gray(
+          source[x+k][y+l],
+          constants.GRAYSCALE_AVERAGE,
+        )
+        gradientX += gray * HORIZONTAL[m][n];
+        gradientY += gray * VERTICAL[m][n];
+      }
+    }
+    const colorCode = 255 - Math.sqrt(gradientX ** 2 + gradientY ** 2);
+    newImageData.data[i] = colorCode;
+    newImageData.data[i + 1] = colorCode;
+    newImageData.data[i + 2] = colorCode;
+    newImageData.data[i + 3] = data[i + 3];
   }
   return newImageData;
 }
