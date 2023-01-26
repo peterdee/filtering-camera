28d13e9bf46924b8e6812aa9f8d8f7dd38f5e437
Peter <p.dyumin@dunice.net>
2023-01-26 20:34:54 +0300

    Adjustments for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type ServerOptions } from 'vite';
 import { readFileSync } from 'node:fs';
 import vue from '@vitejs/plugin-vue';
 
+const serverConfig: ServerOptions = {};
+if (process.env.ENV === 'local') {
+  serverConfig.cors = true;
+  serverConfig.https = {
+    cert: readFileSync('./serve/cert.pem'),
+    key: readFileSync('./serve/key.pem'),
+  };
+  serverConfig.port = 3000;
+}
+
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    cors: true,
-    https: {
-      cert: readFileSync('./serve/cert.pem'),
-      key: readFileSync('./serve/key.pem'),
-    },
-    port: 3000,
-  },
+  server: serverConfig,
 });
