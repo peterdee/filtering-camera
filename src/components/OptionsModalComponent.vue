b855e942cace9af130916123050619e96d9a59d6
Peter <p.dyumin@dunice.net>
2023-02-21 10:41:47 +0300

    Add switch to the Options modal

--- src/components/OptionsModalComponent.vue
+++ src/components/OptionsModalComponent.vue
@@ -78,19 +78,21 @@ const handleThresholdInput = (event: Event): void => {
         <h2 :class="`ns t-center title ${isMobile ? 'title-mobile' : ''}`">
           Camera: image processing in real time
         </h2>
-        <div class="f mt-2 ns">
-          <input
-            class="checkbox"
-            id="switch"
-            type="checkbox"
-            :checked="flipImage"
-            @input="emit('toggle-flip')"
-          />
+        <div class="f ai-center j-space-between ns mt-2">
           <label
-            class="ml-half"
+            class="switch-label"
             for="switch"
           >
             Flip image
+          </label>
+          <label class="switch">
+            <input
+              id="switch"
+              type="checkbox"
+              :checked="flipImage"
+              @input="emit('toggle-flip')"
+            >
+            <span class="slider round" />
           </label>
         </div>
         <span class="mt-1 ns">
@@ -186,5 +188,58 @@ const handleThresholdInput = (event: Event): void => {
 }
 .title-mobile {
   font-size: calc(var(--spacer) + var(--spacer-quarter));
+}
+.switch {
+  display: inline-block;
+  height: calc(var(--spacer) + var(--spacer-half));
+  position: relative;
+  width: calc(var(--spacer) * 3);
+}
+.switch input {
+  height: 0;
+  opacity: 0;
+  width: 0;
+}
+.switch-label {
+  cursor: pointer;
+}
+.slider {
+  background-color: var(--muted);
+  bottom: 0;
+  cursor: pointer;
+  left: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
+  transition: var(--transition);
+  -webkit-transition: var(--transition);
+}
+.slider:before {
+  background-color: var(--background);
+  bottom: var(--spacer-quarter);
+  content: "";
+  height: var(--spacer);
+  left: var(--spacer-quarter);
+  position: absolute;
+  transition: var(--transition);
+  -webkit-transition: var(--transition);
+  width: var(--spacer);
+}
+input:checked + .slider {
+  background-color: var(--accent);
+}
+input:focus + .slider {
+  box-shadow: 0 0 1px var(--accent);
+}
+input:checked + .slider:before {
+  -ms-transform: translateX(calc(var(--spacer) + var(--spacer-half)));
+  -webkit-transform: translateX(calc(var(--spacer) + var(--spacer-half)));
+  transform: translateX(calc(var(--spacer) + var(--spacer-half)));
+}
+.slider.round {
+  border-radius: calc(var(--spacer));
+}
+.slider.round:before {
+  border-radius: 50%;
 }
 </style>
